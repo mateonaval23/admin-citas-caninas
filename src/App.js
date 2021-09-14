56205c127b456bbebfdf4a8575880e3c6cbcdaa2
Matias Lopez Barrios <matias.lopezbarrios@gmail.com>
2021-09-14 19:21:06 -0300

    Se agrega funcionalidad de useEffect y PropTypes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+
+
+  //Obtencion de citas si existen en el local storage
+  let citasInicialesState = JSON.parse(localStorage.getItem('citas'))
+  if(!citasInicialesState) {
+    citasInicialesState = []
+  }
+  // State para el listado de citas creadas
+  const [ citas , guardarCitas ] = useState(citasInicialesState)
+
+  // Vamos a ver otro hook que nos permite ejecutar funcionalidades cuando un state cambia
+
+  useEffect(() => {
+
+        // Se obtienen las citas si existen iniciales y se transforman de cadena a un arreglo de objeto cita
+        let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+    
+        if(citasIniciales){
+          localStorage.setItem('citas', JSON.stringify(citas))
+        }else{
+          localStorage.setItem('citas', JSON.stringify([]))
+        }
+        
+        
+  }, [citas])
+
+
+
+  // Funcion que vamos a pasar a formulario para agregar una cita al array o listado
+  const crearCita =  cita => {
+    guardarCitas([...citas, cita])
+  } 
+
+
+  // 
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+    guardarCitas(nuevasCitas)
+  }
+
+
+  // Mensaje condicional para el titulo de citas
+  const titulo = citas.length === 0 ? 'No hay Citas' : 'Administra tus citas'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
